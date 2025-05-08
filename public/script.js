1af1427253d2c4f0c7557d3a491bb727b36f07d4
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-08 00:44:27 -0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,15 @@
+// frontend/public/js/script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const cobuTech = document.getElementById('cobu-tech');
+    const industry = document.getElementById('industry');
+
+    setTimeout(() => {
+        cobuTech.classList.add('visible');
+        setTimeout(() => {
+            industry.classList.add('visible');
+            setTimeout(() => {
+                window.location.href = '/waiting.html';
+            }, 1000);
+        }, 1000);
+    }, 100);
+});
